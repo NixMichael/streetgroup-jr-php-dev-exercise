cad30d0ded53ef16a7714e1816e0282a9498682c
Michael Nix <mpnix@protonmail.com>
2021-07-27 17:12:47 +0100

    Add voting button to each dog image card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,18 @@
     <div class="w-4/12 lg:w-3/12 p-2">
         <div class="rounded overflow-hidden bg-white border border-gray-200 p-4">
             <img class="w-full h-full" width="400px" src="{{ asset('storage/' . $picture->file_path) }}">
-            <p class="mt-2 text-gray-500">{{ $picture->name }}</p>
-            <p class="mt-2 text-gray-500">{{ $picture->votes }} votes</p>
+            <div class="flex justify-between">
+                <div>
+                    <p class="mt-2 text-gray-500">{{ $picture->name }}</p>
+                    <p class="mt-2 text-gray-500">{{ $picture->votes }} votes</p>
+                </div>
+                <div>
+                    <form action="/pictures/{{$picture->id}}/upvote" method="POST">
+                        @csrf
+                        <input type="submit" value="Vote!" class="cursor-pointer mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 @endforeach
