532864bb77abece7a9c7f156677a16744a5ee587
Michael Nix <mpnix@protonmail.com>
2021-07-27 22:29:12 +0100

    Add error messages to upload form
    - Display error message if name or image not entered
    - Remember value of name field in result of an error
    - Limit width of preview image

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -6,21 +6,32 @@
     <h2 class="text-center text-2xl my-4">Upload your dog!</h2>
     <form class="flex flex-col items-center p-2" action="/pictures" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="flex items-center">
-            <label class="mr-2" for="name">Name: </label>
-            <input class="border p-2" type="text" id="name" name="name">
+
+        <div class="flex flex-col">
+            <div class="flex items-center">
+                <label class="mr-2" for="name">Name: </label>
+                <input class="border p-2" type="text" id="name" name="name" value="{{ old('name') }}">
+            </div>
+                @error('name')
+                    <span class="text-center text-red-600">{{$message}}</span>
+                @enderror
         </div>
         <div class="bg-gray-200 p-4 mt-4 w-full flex flex-col lg:flex-row lg:justify-around items-center">
-            <div class="flex flex-col items-center">
-                <input class="my-4 hidden" type="file" id="image-selection" name="image">
-                <label class="my-4 bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-4 rounded" for="image-selection">Select image</label>
-                <span>
-                    <strong>Chosen file:</strong>
-                    <span id="file-path">None</span>
-                </span>
-            </div>
-            <div class="mt-2 w-auto" id="preview-container">
-                <img src="" id="preview"/>
+            <div class="flex flex-col">
+                <div class="flex flex-col items-center">
+                    <input class="my-4 hidden" type="file" id="image-selection" name="image">
+                    <label class="my-4 bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-4 rounded" for="image-selection">Select image</label>
+                    <span>
+                        <strong>Chosen file:</strong>
+                        <span id="file-path">None</span>
+                    </span>
+                </div>
+                <div class="mt-2 w-auto max-w-lg" id="preview-container">
+                    <img src="" id="preview"/>
+                </div>
+                @error('image')
+                    <span class="text-center text-red-600">{{$message}}</span>
+                @enderror
             </div>
         </div>
         <input type="submit" value="Upload" class="m-4 bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-4 rounded">
