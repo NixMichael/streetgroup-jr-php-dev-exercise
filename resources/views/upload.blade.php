6ff6c107c52045c64f012847ca8120dfab202aa0
Michael Nix <mpnix@protonmail.com>
2021-07-27 15:40:58 +0100

    Add form to upload view

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -4,7 +4,12 @@
 
 <div class="bg-white p-4 w-2/3 mx-auto">
     <h2 class="text-center text-2xl">Upload your dog!</h2>
-    <p>Please add your form code here</p>
+    <form class="flex flex-col items-center p-4" action="/pictures" method="POST" enctype="multipart/form-data">
+        @csrf
+        <input class="border p-2" type="text" name="name" placeholder="Name...">
+        <input class="m-4" type="file" name="image">
+        <input type="submit" value="Upload" class="m-4 bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-4 rounded">
+    </form>
 </div>
 
 @endsection
